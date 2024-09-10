478cc8b5614b8e800f086e8066fef67d936f2d1a
Tim Spratt <timspratt@hotmail.co.uk>
2024-09-11 00:45:53 +0100

    wip

--- ngx_http_js_challenge.c
+++ ngx_http_js_challenge.c
@@ -153,6 +153,31 @@ static char *ngx_http_js_challenge_merge_loc_conf(ngx_conf_t *cf, void *parent, 
     ngx_conf_merge_str_value(conf->html_path, prev->html_path, NULL)
     ngx_conf_merge_str_value(conf->title, prev->title, DEFAULT_TITLE)
 
+    ngx_flag_t is_enabled = conf->enabled;
+
+    // If a variable name was passed instead of a flag
+    if (conf->enabled_variable_name.len > 0) {
+        ngx_str_t variable_value;
+
+        // Get the value of the variable
+        ngx_uint_t key = ngx_hash_strlow(conf->enabled_variable_name.data, conf->enabled_variable_name.data, conf->enabled_variable_name.len);
+        ngx_http_variable_value_t *var = ngx_http_get_variable(r, &conf->enabled_variable_name, key);
+
+        if (var == NULL || var->not_found) {
+            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "[ js challenge log ] variable not found or empty");
+            return NGX_DECLINED;
+        }
+
+        variable_value.data = var->data;
+        variable_value.len = var->len;
+
+        if (ngx_strncmp(variable_value.data, "on", variable_value.len) == 0) {
+            is_enabled = 1;
+        } else {
+            is_enabled = 0;
+        }
+    }
+
     if (conf->bucket_duration < 1) {
         ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "bucket_duration must be equal or more than 1");
         return NGX_CONF_ERROR;
@@ -160,7 +185,7 @@ static char *ngx_http_js_challenge_merge_loc_conf(ngx_conf_t *cf, void *parent, 
 
     if (conf->html_path.data == NULL) {
         conf->html = NULL;
-    } else if (conf->enabled) {
+    } else if (is_enabled) {
 
         // Read file in memory
         char path[PATH_MAX];
